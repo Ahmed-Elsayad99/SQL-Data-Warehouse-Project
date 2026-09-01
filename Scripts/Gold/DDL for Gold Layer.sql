1b967c2348ee56a822ad614ebb9482c03f9d6b92
Ahmed Elsayad <elsayad427665@gmail.com>
2026-09-01 23:30:41 +0300

    Add files via upload

---
+++ Scripts/Gold/DDL for Gold Layer.sql
@@ -0,0 +1,153 @@
+/*
+===============================================================================
+DDL Script: Create Gold Tables
+===============================================================================
+Script Purpose:
+    This script creates Tables for the Gold layer in the data warehouse. 
+    The Gold layer represents the final dimension and fact tables (Galaxy Schema)
+
+    Each Table performs transformations and combines data from the Silver layer 
+    to produce a clean, enriched, and business-ready dataset.
+
+Usage:
+    - These Tables can be queried directly for analytics and reporting.
+===============================================================================
+*/
+
+-- =============================================================================
+-- Create Dimension: gold.dim_customers
+-- =============================================================================
+
+CREATE TABLE gold.dim_customers (
+    customer_sk BIGINT IDENTITY(1,1) PRIMARY KEY,
+    customer_id VARCHAR(50),
+    customer_unique_id VARCHAR(50),
+    zip_code INT,
+    city VARCHAR(50),
+    state VARCHAR(50),
+    dwh_create_date DATETIME
+);
+
+
+-- =============================================================================
+-- Create Dimension: gold.dim_location
+-- =============================================================================
+
+CREATE TABLE gold.dim_location (
+    location_sk BIGINT IDENTITY(1,1) PRIMARY KEY,
+    zip_code INT,
+    latitude DECIMAL(10,6),
+    longitude DECIMAL(10,6),
+    city VARCHAR(50),
+    state VARCHAR(50),
+    dwh_create_date DATETIME
+);
+
+-- =============================================================================
+-- Create Dimension: gold.dim_sellers
+-- =============================================================================
+
+CREATE TABLE gold.dim_sellers (
+    seller_sk BIGINT IDENTITY(1,1) PRIMARY KEY,
+    seller_id VARCHAR(50),
+    zip_code INT,
+    city VARCHAR(50),
+    state VARCHAR(50),
+    dwh_create_date DATETIME
+);
+
+
+
+-- =============================================================================
+-- Create Dimension: gold.dim_product_categories
+-- =============================================================================
+
+CREATE TABLE gold.dim_product_categories (
+    product_category_id BIGINT PRIMARY KEY,
+    product_category_name VARCHAR(50),
+    product_category_name_engilsh VARCHAR(50),
+    dwh_create_date DATETIME
+);
+
+
+-- =============================================================================
+-- Create Dimension: gold.dim_products
+-- =============================================================================
+
+CREATE TABLE olist_dwh.gold.dim_products (
+    product_sk BIGINT IDENTITY(1,1) PRIMARY KEY,
+    product_id VARCHAR(50) NOT NULL,
+    product_category_id BIGINT,
+    weight_g INT,
+    length_cm INT,
+    height_cm INT,
+    width_cm INT,
+    dwh_create_date DATETIME
+);
+
+-- =============================================================================
+-- Create Fact: gold.fact_orders
+-- =============================================================================
+
+
+CREATE TABLE gold.fact_orders (
+	order_sk BIGINT IDENTITY(1,1) PRIMARY KEY,
+    order_id VARCHAR(50) NOT NULL,
+	customer_sk BIGINT NOT NULL,
+	order_status VARCHAR(50),
+	purchase_date DATE,
+	approved_date DATE,
+	delivered_carrier_date DATE,
+	expected_delivery_date DATE,
+	delivered_customer_date DATE,
+	dwh_create_date datetime
+);
+
+
+-- =============================================================================
+-- Create Fact: gold.fact_order_items
+-- =============================================================================
+
+CREATE TABLE gold.fact_order_items (
+    order_item_sk BIGINT IDENTITY(1,1) PRIMARY KEY,
+    order_sk BIGINT NOT NULL,
+    order_item_id INT NOT NULL,
+    product_sk BIGINT NOT NULL,
+    seller_sk BIGINT NOT NULL,
+    shipping_limit_date DATE,
+    price DECIMAL(10,2),
+    freight_value DECIMAL(10,2),
+    dwh_create_date DATETIME
+);
+
+
+-- =============================================================================
+-- Create Fact: gold.fact_order_payments
+-- =============================================================================
+
+CREATE TABLE gold.fact_order_payments (
+    order_payment_sk BIGINT IDENTITY(1,1) PRIMARY KEY,
+    order_sk BIGINT NOT NULL,
+    payment_sequential INT,
+    payment_type VARCHAR(50),
+    payment_installments INT,
+    payment_value DECIMAL(10,2),
+    dwh_create_date DATETIME
+);
+
+
+-- =============================================================================
+-- Create Fact: gold.fact_order_reviews
+-- =============================================================================
+
+CREATE TABLE gold.fact_order_reviews (
+    order_review_sk BIGINT IDENTITY(1,1) PRIMARY KEY,
+    review_id VARCHAR(50) NOT NULL,
+    order_sk BIGINT NOT NULL,
+    review_score INT,
+    review_creation_date DATE,
+    review_answer_date DATE,
+    dwh_create_date DATETIME
+);
+
+
---
+++ Scripts/Gold/ETL for Gold Layer.sql
@@ -0,0 +1,270 @@
+/*
+===============================================================================
+ETL Script: Create Automated ETL Pipelines Process
+===============================================================================
+Script Purpose:
+    This script creates Automated ETL Pipelines Process in the 'Gold' schema
+===============================================================================
+*/
+
+EXEC gold.load_gold;
+GO
+
+CREATE OR ALTER PROCEDURE gold.load_gold AS
+BEGIN
+Print '=====================================================';
+Print 'Loading Gold Layer';
+Print '=====================================================';
+
+Print '-----------------------------------------------------';
+Print 'Loading Dimension Tables';
+Print '-----------------------------------------------------';
+
+Print 'Truncating Table: olist_dwh.gold.dim_customers';
+Truncate Table olist_dwh.gold.dim_customers;
+Print 'Inserting Data Into: olist_dwh.gold.dim_customers';
+
+
+INSERT INTO olist_dwh.gold.dim_customers (
+    customer_id,
+    customer_unique_id,
+    zip_code,
+    city,
+    state,
+    dwh_create_date
+    )
+
+SELECT
+    customer_id,
+    customer_unique_id,
+    customer_zip_code_prefix,
+    customer_city,
+    customer_state,
+    dwh_create_date
+FROM olist_dwh.silver.crm_customers;
+
+
+-- ================================================================================================== --
+
+Print 'Truncating Table: olist_dwh.gold.dim_location';
+Truncate Table olist_dwh.gold.dim_location;
+Print 'Inserting Data Into: olist_dwh.gold.dim_location';
+
+
+INSERT INTO olist_dwh.gold.dim_location (
+    zip_code,
+    latitude,
+    longitude,
+    city,
+    state,
+    dwh_create_date 
+    )
+
+SELECT
+    geolocation_zip_code_prefix,
+    geolocation_lat,
+    geolocation_lng,
+    geolocation_city,
+    geolocation_state,
+    dwh_create_date
+FROM olist_dwh.silver.crm_geo_location;
+
+
+-- ================================================================================================== --
+
+Print 'Truncating Table: olist_dwh.gold.dim_sellers';
+Truncate Table olist_dwh.gold.dim_sellers;
+Print 'Inserting Data Into: olist_dwh.gold.dim_sellers';
+
+INSERT INTO olist_dwh.gold.dim_sellers (
+    seller_id,
+    zip_code,
+    city,
+    state,
+    dwh_create_date 
+    )
+
+SELECT
+    seller_id,
+    seller_zip_code_prefix,
+    seller_city,
+    seller_state,
+    dwh_create_date
+FROM olist_dwh.silver.csv_sellers;
+
+
+-- ================================================================================================== --
+
+Print 'Truncating Table: olist_dwh.gold.dim_product_categories';
+Truncate Table olist_dwh.gold.dim_product_categories;
+Print 'Inserting Data Into: olist_dwh.gold.dim_product_categories';
+
+INSERT INTO olist_dwh.gold.dim_product_categories (
+    product_category_id,
+    product_category_name,
+    product_category_name_engilsh,
+    dwh_create_date 
+    )
+
+SELECT
+    product_category_id,
+    product_category_name,
+    product_category_name_english,
+    dwh_create_date
+FROM olist_dwh.silver.csv_product_categories;
+
+-- ================================================================================================== --
+
+Print 'Truncating Table: olist_dwh.gold.dim_products';
+Truncate Table olist_dwh.gold.dim_products;
+Print 'Inserting Data Into: olist_dwh.gold.dim_products';
+
+INSERT INTO olist_dwh.gold.dim_products (
+    product_id,
+    product_category_id,
+    weight_g,
+    length_cm,
+    height_cm,
+    width_cm,
+    dwh_create_date 
+    )
+
+SELECT
+    p.product_id,
+    pc.product_category_id,
+    p.product_weight_g,
+    p.product_length_cm,
+    p.product_height_cm,
+    p.product_width_cm,
+    p.dwh_create_date
+FROM olist_dwh.silver.csv_products p
+LEFT JOIN olist_dwh.gold.dim_product_categories pc
+    ON  p.product_category_name = pc.product_category_name;
+
+
+-- ================================================================================================== --
+
+Print '-----------------------------------------------------';
+Print 'Loading Fact Tables';
+Print '-----------------------------------------------------';
+
+Print 'Truncating Table: olist_dwh.gold.fact_orders';
+Truncate Table olist_dwh.gold.fact_orders;
+Print 'Inserting Data Into: olist_dwh.gold.fact_orders';
+
+
+INSERT INTO olist_dwh.gold.fact_orders (
+    order_id,
+	customer_sk,
+	order_status,
+	purchase_date,
+	approved_date,
+	delivered_carrier_date,
+	expected_delivery_date,
+	delivered_customer_date,
+	dwh_create_date
+	)
+SELECT 
+    o.order_id,
+	c.customer_sk,
+	o.order_status,
+	CAST(o.order_purchase_timestamp AS DATE) AS purchase_date,
+	CAST(o.order_approved_at AS DATE) AS approved_date,
+	CAST(o.order_delivered_carrier_date AS DATE) AS delivered_carrier_date,
+	CAST(o.order_estimated_delivery_date AS DATE) AS expected_delivery_date,
+	CAST(o.order_delivered_customer_date AS DATE) AS delivered_customer_date,
+    o.dwh_create_date
+FROM olist_dwh.silver.erp_orders o
+LEFT JOIN olist_dwh.gold.dim_customers c
+    on o.customer_id = c.customer_id ;
+
+
+-- ================================================================================================== --
+
+Print 'Truncating Table: olist_dwh.gold.fact_order_items';
+Truncate Table olist_dwh.gold.fact_order_items;
+Print 'Inserting Data Into: olist_dwh.gold.fact_order_items';
+
+INSERT INTO olist_dwh.gold.fact_order_items (
+    order_sk,
+    order_item_id,
+    product_sk,
+    seller_sk,
+    shipping_limit_date,
+    price,
+    freight_value,
+    dwh_create_date 
+    )
+
+SELECT
+    o.order_sk,
+    oi.order_item_id,
+    p.product_sk,
+    s.seller_sk,
+    CAST(oi.shipping_limit_date AS DATE) AS shipping_limit_date,
+    oi.price,
+    oi.freight_value,
+    oi.dwh_create_date
+FROM olist_dwh.silver.erp_order_items oi
+LEFT JOIN olist_dwh.gold.fact_orders o
+    ON  oi.order_id = o.order_id
+LEFT JOIN olist_dwh.gold.dim_products p
+    ON oi.product_id = p.product_id
+LEFT JOIN olist_dwh.gold.dim_sellers s
+    ON oi.seller_id = s.seller_id;
+
+
+-- ================================================================================================== --
+
+Print 'Truncating Table: olist_dwh.gold.fact_order_payments';
+Truncate Table olist_dwh.gold.fact_order_payments;
+Print 'Inserting Data Into: olist_dwh.gold.fact_order_payments';
+
+INSERT INTO olist_dwh.gold.fact_order_payments (
+    order_sk,
+    payment_sequential,
+    payment_type,
+    payment_installments,
+    payment_value,
+    dwh_create_date 
+    )
+
+SELECT
+    o.order_sk,
+    op.payment_sequential,
+    op.payment_type,
+    op.payment_installments,
+    op.payment_value,
+    op.dwh_create_date
+FROM olist_dwh.silver.erp_order_payments op
+LEFT JOIN olist_dwh.gold.fact_orders o
+    ON  op.order_id = o.order_id;
+
+
+-- ================================================================================================== --
+
+Print 'Truncating Table: olist_dwh.gold.fact_order_reviews';
+Truncate Table olist_dwh.gold.fact_order_reviews;
+Print 'Inserting Data Into: olist_dwh.gold.fact_order_reviews';
+
+INSERT INTO olist_dwh.gold.fact_order_reviews (
+    review_id,
+    order_sk,
+    review_score,
+    review_creation_date,
+    review_answer_date,
+    dwh_create_date 
+    )
+
+SELECT
+    ov.review_id,
+    o.order_sk,
+    ov.review_score,
+    CAST(ov.review_creation_date AS DATE) review_creation_date,
+    CAST(ov.review_answer_timestamp AS DATE) review_answer_date,
+    ov.dwh_create_date
+FROM olist_dwh.silver.erp_order_reviews ov
+LEFT JOIN olist_dwh.gold.fact_orders o
+    ON  ov.order_id = o.order_id;
+
+END;
